a6af63d7089a234489f974e23954101f86a9d1f5
Arjun Venkataswamy <avenkat2@gmail.com>
2013-03-01 15:01:06 -0600

    adds enlistment create action

--- app/controllers/enlistments_controller.rb
+++ app/controllers/enlistments_controller.rb
@@ -1,6 +1,23 @@
 class EnlistmentsController < ApplicationController
 
+  def create
+    lab = Lab.find(params['enlistment']['lab_id'])
+    user = User.find(params['enlistment']['user_id'])
 
+    enlistment = Enlistment.new
+    enlistment.lab_id = lab.id
+    enlistment.user_id = user.id
+    if enlistment.save
+      lab.tasks.each do |task|
+        EnlistmentTask.create(enlistment_id: enlistment.id,
+                              task_id: task.id)
+      end
+      redirect_to enlistment
+    else
+      redirect_to user, notice: "There was a problem enrolling you in the lab"
+    end
+
+  end
 
   def show
     @enlistment = Enlistment.find(params[:id])
